c8349b982e118a4d5548a58e60f36ca4fe91a4b7
Michael-Yan <956462326@qq.com>
2017-09-02 21:22:10 +0800

    （1）替换app图标（2）添加启动页(闪屏页）

--- app/src/main/java/com/avtc/vcampus/MainActivity.java
+++ app/src/main/java/com/avtc/vcampus/MainActivity.java
@@ -1,28 +1,13 @@
 package com.avtc.vcampus;
 
-import android.os.Build;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.ViewGroup;
-import android.view.WindowManager;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        smoothSwitchScreen();
         setContentView(R.layout.activity_main);
-    }
-
-    private void smoothSwitchScreen() {
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
-            ViewGroup rootView = ((ViewGroup) this.findViewById(android.R.id.content));
-            int resourceId = getResources().getIdentifier("status_bar_height", "dimen", "android");
-            int statusBarHeight = getResources().getDimensionPixelSize(resourceId);
-            rootView.setPadding(0, statusBarHeight, 0, 0);
-            getWindow().addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN);
-            getWindow().addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
-        }
     }
 }
